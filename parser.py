d8fbead3844a408d9c7bc2ffc6c42049acc46c9d
William Hopkins <hopkinwe@mail.uc.edu>
2020-08-05 20:18:39 -0400

    Proper implementation of setParseAction for each parserElement

--- parser.py
+++ parser.py
@@ -3,25 +3,15 @@ from pyparsing import alphas, nums
 from pyparsing import replaceWith, Suppress, restOfLine, ZeroOrMore
 
 
-def replace(tokens, target, replacement):
-    for t in tokens:
-        if t == target:
-            tokens[t] = replacement
-
-    return tokens
-
-
 def generateJSONTokens():
-    stringDblQuotes = Word(alphas) + "=" + "\"" + Word(alphas) + "\""
-    stringDblQuotes = stringDblQuotes.setParseAction(lambda s, l, t: replace(t, "=", ":"))
-    # stringDblQuotes = Literal("=").addParseAction(replaceWith(":"))
-    # stringDblQuotes = stringDblQuotes.setParseAction(lambda s, l, t: t.append('\n'))
+    stringDblQuotes = Word(alphas) + Literal("=").addParseAction(replaceWith(":")) + "\"" + Word(alphas) + "\""
+    stringDblQuotes = stringDblQuotes.setParseAction(lambda s, l, t: t.append('\n'))
 
-    stringNoQuotes = Word(alphas) + "=" + Word(alphas)
-    # stringNoQuotes = stringNoQuotes.setParseAction(lambda s, l, t: t.append('\n'))
+    stringNoQuotes = Word(alphas) + Literal("=").addParseAction(replaceWith(":")) + Word(alphas)
+    stringNoQuotes = stringNoQuotes.setParseAction(lambda s, l, t: t.append('\n'))
 
-    dateDblQuotes = Word(alphas) + "=" + "\"" + Word(nums) + "." + Word(nums) + "." + Word(nums) + "." + Word(nums) + "\""
-    # dateDblQuotes = dateDblQuotes.setParseAction(lambda s, l, t: t.append('\n'))
+    dateDblQuotes = Word(alphas) + Literal("=").addParseAction(replaceWith(":")) + "\"" + Word(nums) + "." + Word(nums) + "." + Word(nums) + "." + Word(nums) + "\""
+    dateDblQuotes = dateDblQuotes.setParseAction(lambda s, l, t: t.append('\n'))
 
     # comment = Suppress("#") + Suppress(restOfLine) - PROBABLY UNNECESSARY
     comment = "#" + restOfLine
@@ -32,8 +22,9 @@ def generateJSONTokens():
 
     save_file = open("mock_save.txt", "r")
 
-    save_tokens = save.parseFile(save_file)
-    print(save_tokens)
+    tokens = save.parseFile(save_file)
+    print(tokens)
+    return tokens
 
 
 def writeTokensToFile(tokens):
@@ -44,4 +35,4 @@ def writeTokensToFile(tokens):
         output_file.write(token)
 
 
-generateJSONTokens()
+writeTokensToFile(generateJSONTokens())
