94b4a2c266b4257b288023265c25fd49be646790
William Hopkins <hopkinwe@mail.uc.edu>
2020-08-06 02:56:05 -0400

    Began implementation of paradoxObject parseElement

--- parser.py
+++ parser.py
@@ -4,34 +4,57 @@ from pyparsing import replaceWith, Suppress, restOfLine, ZeroOrMore
 
 
 def generateJSONTokens():
-    stringDblQuotes = Word(alphas).addParseAction(
-        lambda s, l, t: (t := ("{\"" + t[0] + "\""))
+    stringDblQuotes = Word(alphas + nums).addParseAction(
+        lambda s, l, t: (t := ("\"" + t[0] + "\""))
     ) + Literal("=").addParseAction(
         replaceWith(":")
-    ) + Literal("\"") + Word(alphas).addParseAction(
+    ) + Literal("\"") + Word(alphas + nums).addParseAction(
         lambda s, l, t: (t := ("\'" + t[0] + "\'"))
     ) + Literal("\"").addParseAction(
-        replaceWith("\"}")
+        replaceWith("\"")
     ) + ~Literal("=")
 
-    stringDblQuotes = stringDblQuotes.setParseAction(lambda s, l, t: t.append('\n'))
+    stringDblQuotes = stringDblQuotes.setParseAction(lambda s, l, t: t.append(',\n'))
 
-    # lambda explained: s[l] instead of t is string[location] instead of token to avoid maximum recursion errors
+    # lambda explained: t[0] to access string inside token instead of t because t is the token, which includes
+    # the lambda, which includes the token, which includes the lambda... max recursion error
 
-    stringNoQuotes = Word(alphas).addParseAction(
-        lambda s, l, t: (t := ("{\"" + s[l] + "\""))
+    stringNoQuotes = Word(alphas + nums).addParseAction(
+        lambda s, l, t: (t := ("\"" + t[0] + "\""))
     ) + Literal("=").addParseAction(
         replaceWith(":")
-    ) + Word(alphas) + ~Literal("=")
-    stringNoQuotes = stringNoQuotes.setParseAction(lambda s, l, t: t.append('\n'))
+    ) + Word(alphas + nums).addParseAction(
+        lambda s, l, t: (t := ("\"" + t[0] + "\""))
+    ) + ~Literal("=")
+    stringNoQuotes = stringNoQuotes.setParseAction(lambda s, l, t: t.append(',\n'))
 
-    dateDblQuotes = Word(alphas) + Literal("=").addParseAction(replaceWith(":")) + "\"" + Word(nums) + "." + Word(nums) + "." + Word(nums) + "." + Word(nums) + "\""
-    dateDblQuotes = dateDblQuotes.setParseAction(lambda s, l, t: t.append('\n'))
+    dateDblQuotes = Word(alphas + nums).addParseAction(
+        lambda s, l, t: (t := ("\"" + t[0] + "\""))
+    ) + Literal("=").addParseAction(
+        replaceWith(":")
+    ) + Literal("\"").addParseAction(
+        replaceWith("\"'")
+    ) + Word(nums + ".") + Literal("\"").addParseAction(
+        replaceWith("\'\"")
+    )
+    dateDblQuotes = dateDblQuotes.setParseAction(lambda s, l, t: t.append(',\n'))
+
+    # paradoxObject = Word(alphas).addParseAction(
+    #     lambda s, l, t: (t := ("\"" + t[0] + "\""))
+    # ) + Literal("=").addParseAction(
+    #     replaceWith(":")
+    # ) + "{" + (stringDblQuotes ^ stringNoQuotes ^ dateDblQuotes) + "}"
+
+    paradoxObject = Word(alphas).addParseAction(
+        lambda s, l, t: (t := ("\"" + t[0] + "\""))
+    ) + Literal("=").addParseAction(
+        replaceWith(":")
+    ) + "{" + restOfLine
 
     # comment = Suppress("#") + Suppress(restOfLine) - PROBABLY UNNECESSARY
     comment = "#" + restOfLine
 
-    save = ZeroOrMore(stringDblQuotes) & ZeroOrMore(stringNoQuotes) & ZeroOrMore(dateDblQuotes)
+    save = ZeroOrMore(stringDblQuotes) & ZeroOrMore(stringNoQuotes) & ZeroOrMore(dateDblQuotes) & ZeroOrMore(paradoxObject)
 
     save.ignore(comment)
 
